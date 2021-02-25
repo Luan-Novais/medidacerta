ddec9fe7ac646793916f606541dc72fbc97b2f45
Luan-Novais <luan.novaiss@outlook.com>
2021-02-25 08:27:02 -0300

    validando formulario

--- js/script.js
+++ js/script.js
@@ -3,29 +3,33 @@ const fields = document.querySelectorAll("[required]")
 
 
 function customValidation(event) {
+    event.preventDefault()
     const field = event.target
 
     function verifyErrors() {
         let foundError = false;
 
         for (const error in field.validity) {
-            if (error !== "customError" && field.validity[error]) {
+            if (field.validity[error] && !field.validity.valid) {
                 foundError = error
             }
         }
-
+        console.log(foundError)
         return foundError
     }
 
     const error = verifyErrors()
-    console.log(error)
+
+    const divError = field.parentNode.querySelector('.error')
+   
+
 
     if (error) {
-        field.setCustomValidity("Campo obrigatório")
-
+        divError.classList.add("active")
+        divError.innerHTML = "Campo obrigatório"
     } else {
-        field.setCustomValidity("")
-
+        divError.innerHTML = ""
+        divError.classList.remove("active")
     }
 
 }
@@ -33,6 +37,7 @@ function customValidation(event) {
 
 for (field of fields) {
     field.addEventListener("invalid", customValidation)
+    field.addEventListener("blur", customValidation)
 }
 
 
@@ -74,43 +79,6 @@ function calculaImc(){
 }
 
 
-// function calculaImc(){
-//         document.querySelector('.calcular').addEventListener('click', (event) =>{
-//         event.preventDefault();
-//         let imc;
-//         const altura =  document.querySelector('#altura').value;
-//         const peso =  document.querySelector('#peso').value;
-//         imc =  peso / (altura * altura)
-//         console.log(imc)
-//         document.querySelector('.calculoImc').innerHTML = `Meu IMC é: ${imc.toFixed(2)}`;
-//         document.querySelector('.calculoImc').style.display = 'block'
-
-//         if(altura || peso == NaN){
-//             limparCampos();
-//         }
-
-
-//         if(imc.toFixed(2) <= 18.5){
-//             document.querySelector('#magreza').style.backgroundColor = "rgb(93, 250, 101)";
-//             document.querySelector('.calculoImc').style.backgroundColor = "rgb(93, 250, 101)";
-//         } else if (imc  >= 18.5 && imc <= 24.9) {
-//             document.querySelector('#normal').style.backgroundColor = "rgb(93, 250, 101)";
-//             document.querySelector('.calculoImc').style.backgroundColor = "rgb(93, 250, 101)";
-//         } else if (imc >= 25.0 && imc <= 29.9) {
-//             document.querySelector('#sobrepeso').style.backgroundColor = "rgb(241, 231, 78)";
-//             document.querySelector('.calculoImc').style.backgroundColor = "rgb(241, 231, 78)";
-//         } else if (imc >= 30.0 && imc <= 39.9) {
-//             document.querySelector('#obesidade').style.backgroundColor = "rgb(245, 130, 35";
-//             document.querySelector('.calculoImc').style.backgroundColor = "rgb(245, 130, 35)";
-//         } else {
-//             document.querySelector('#obesidadeGrave').style.backgroundColor = "rgb(240, 28, 28)";
-//             document.querySelector('.calculoImc').style.backgroundColor = "rgb(240, 28, 28)";
-//         }
-
-//     })
-// }
-
-
 function limparCampos() {
     document.querySelector('.limpar').addEventListener('click', (event) => {
         event.preventDefault();
@@ -124,6 +92,7 @@ function limparCampos() {
         document.querySelector('#sobrepeso').style.backgroundColor = "white";
         document.querySelector('#obesidade').style.backgroundColor = "white";
         document.querySelector('#obesidadeGrave').style.backgroundColor = "white";
+
         calculaImc();
 
     })
